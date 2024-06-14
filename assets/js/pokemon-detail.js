da0b6445f87db77d6f6fa855ad3602f8988e3760
josedesignerbr@hotmail.com <imjosephe>
2024-06-14 16:26:51 -0400

    inicial da pagina de detalhes do pokemon

--- assets/js/pokemon-detail.js
+++ assets/js/pokemon-detail.js
@@ -8,36 +8,33 @@ load()
 function load() {
     pokemon = JSON.parse(serializedData)
     console.log(pokemon)
-    const newHtml = `<div class="pokemon">
-                        <img class="image" src="${pokemon.photo}" 
+    const newHtml = `<div class="pokemon ${pokemon.type}">
+                        <img class="image ${pokemon.type}" src="${pokemon.photo}" 
                             alt="${pokemon.name}">
 
                         <h1 class="title">${pokemon.name}</h1>
                         <h2 class="number">#${pokemon.number.toString().padStart(3, "0")}</h2>
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol> 
 
-                        <div class="detail">
-                            <ol class="types">
-                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                            </ol>
-                            <div class="medidas">
-                                <div>
-                                    <div class="medidasImageValue">
-                                        <img class="medidasImage" src="https://raw.githubusercontent.com/imjosephe/Pokedex/811c55e0ed3314f7504222a26ca4bf0ec3e08649/assets/icons/weight-svgrepo-com.svg" 
-                                            alt="Balance">
-                                        <h1 class="medidasValue">${pokemon.weight} kg</h1>
-                                    </div>
-                                    <h1 class="medidasTitle">Weight</h1>
+                        <div class="medidas">
+                            <div class="medidasItem">
+                                <div class="medidasImageValue">
+                                    <img class="medidasImage" src="https://raw.githubusercontent.com/imjosephe/Pokedex/fca694e3d0db69579df4d599a950d520c6bc0de4/assets/icons/weight-svgrepo-com.svg" 
+                                        alt="Balance">
+                                    <h1 class="medidasValue">${pokemon.weight} kg</h1>
                                 </div>
-                                <hr class="divider">
-                                <div>
-                                    <div class="medidasImageValue">
-                                        <img class="medidasImage" src="https://raw.githubusercontent.com/imjosephe/Pokedex/811c55e0ed3314f7504222a26ca4bf0ec3e08649/assets/icons/ruler-svgrepo-com.svg" 
-                                            alt="Ruler">
-                                        <h1 class="medidasValue">${pokemon.height} m</h1>
-                                    </div>
-                                    
-                                    <h2 class="medidasTitle">Height</h2>
-                                </div>
+                                <h1 class="medidasTitle">Weight</h1>
+                            </div>
+                            <hr class="divider">
+                            <div class="medidasItem">
+                                <div class="medidasImageValue">
+                                    <img class="medidasImage" src="https://raw.githubusercontent.com/imjosephe/Pokedex/fca694e3d0db69579df4d599a950d520c6bc0de4/assets/icons/ruler-svgrepo-com.svg" 
+                                        alt="Ruler">
+                                    <h1 class="medidasValue">${pokemon.height} m</h1>
+                                </div>                                    
+                                <h2 class="medidasTitle">Height</h2>
                             </div>
                         </div>
                     </div>`                    
